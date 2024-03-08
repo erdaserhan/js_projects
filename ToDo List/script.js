31c0128806b7f24f88c12330614c2cb0cf286248
erdaserhan <erhan209@gmail.com>
2024-03-08 15:34:06 +0100

    js ok

--- ToDo List/script.js
+++ ToDo List/script.js
@@ -1,6 +1,8 @@
 let items = [];
 
 const itemsDiv = document.getElementById("items")
+const input = document.getElementById("itemInput")
+const storageKey = "items"
 
 function renderItems() {
     itemsDiv.innerHTML = null; 
@@ -11,28 +13,48 @@ function renderItems() {
         container.style.marginBottom = "10px"
 
         const text = document.createElement("p")
+        text.style.display = "inline"
+        text.style.marginRight = "20px"
         text.textContent = item;
 
         const button = document.createElement("button")
         button.textContent = "Delete";
         button.onclick = () => removeItem(idx) // ()=> call this function in another function. Otherwise it is gonna call the removeItem functon automatically.
 
-        itemsDiv.appendChild(text)
+        container.appendChild(text)
+        container.appendChild(button)
+
+        itemsDiv.appendChild(container)
     }
 }
 
 function loadItems() {
-
+    const oldItems = localStorage.getItem(storageKey)
+    if(oldItems) items = JSON.parse(oldItems) // We are converting the oldItems to a JS object which is an array here
+    renderItems()
 }
 
 function saveItems() {
-    
+    const stringItems = JSON.stringify(items); //We are converting the items to a string
+    localStorage.setItem(storageKey, stringItems)// storageKey = items
 }
 
 function addItem() {
-    
+    const value = input.value;
+    if(!value){
+        alert("You cannot add an empty item")
+        return
+    }
+    items.push(value)
+    renderItems()
+    input.value = ""
+    saveItems()
 }
 
 function removeItem(idx) {
-    
+    items.splice(idx, 1) // The splice() method adds and/or removes array elements.
+    renderItems()
+    saveItems()
 }
+
+document.addEventListener("DOMContentLoaded", loadItems)
